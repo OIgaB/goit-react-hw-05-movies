0f0246e09f26c4d90031bceb96884102e2c76b2b
OIgaB <57020231+OIgaB@users.noreply.github.com>
2023-06-03 12:12:29 +0000

    Deploying to gh-pages from @ OIgaB/goit-react-hw-05-movies@353740f0dec81844692dc680f8abbc5159fb60cb 🚀

---
+++ static/js/642.d7a9ace9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[642],{642:function(e,t,n){n.r(t);var o=n(184);t.default=function(){console.log("\u0430\u0433\u043e\u0432 Movies!");return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("div",{children:"\u0426\u0435 Movies"}),(0,o.jsxs)("form",{children:[(0,o.jsx)("input",{type:"text",onChange:function(e){var t=e.target.value;console.log(t)}}),(0,o.jsx)("button",{type:"submit",children:"Search"})]})]})}}}]);
+//# sourceMappingURL=642.d7a9ace9.chunk.js.map
